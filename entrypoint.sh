9098d4951a0e89704672c083f24ca0b8958dd7eb
Disyer <daniel@tohka.us>
2024-03-23 14:16:05 +0200

    ping: Add ping URL

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
+set -e
 
 # Import our GPG key
 gpg --import gpg.key
 
 # Run the actual program
 python -m kernelcollector.main
+
+# Ping the URL if it is set
+if [[ -n "$PING_URL" ]]; then
+    curl -m 10 --retry 5 "$PING_URL"
+fi
